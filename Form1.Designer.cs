4fe80e714398ac90a035e256deb2e7501c090874
大💻 <helpdesk>
2023-12-03 22:09:06 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace PDFWatermark
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btMark = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialogPDF = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbPdfFiles = new System.Windows.Forms.ListBox();
+            this.btOpenfile = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbPdfFiles);
+            this.groupBox1.Controls.Add(this.btMark);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btOpenfile);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(476, 245);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "工作区";
+            // 
+            // btMark
+            // 
+            this.btMark.Location = new System.Drawing.Point(8, 216);
+            this.btMark.Name = "btMark";
+            this.btMark.Size = new System.Drawing.Size(462, 23);
+            this.btMark.TabIndex = 5;
+            this.btMark.Text = "开始添加水印";
+            this.btMark.UseVisualStyleBackColor = true;
+            this.btMark.Click += new System.EventHandler(this.btMark_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "请选择 PDF：";
+            // 
+            // openFileDialogPDF
+            // 
+            this.openFileDialogPDF.Filter = "所有 PDF 文件|*.pdf";
+            this.openFileDialogPDF.Multiselect = true;
+            this.openFileDialogPDF.Title = "请选择需要加水印的PDF文件";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(13, 264);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(476, 82);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "说明";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "测试软件，pdf 添加水印。";
+            // 
+            // lbPdfFiles
+            // 
+            this.lbPdfFiles.FormattingEnabled = true;
+            this.lbPdfFiles.ItemHeight = 12;
+            this.lbPdfFiles.Location = new System.Drawing.Point(8, 42);
+            this.lbPdfFiles.Name = "lbPdfFiles";
+            this.lbPdfFiles.Size = new System.Drawing.Size(462, 160);
+            this.lbPdfFiles.TabIndex = 6;
+            // 
+            // btOpenfile
+            // 
+            this.btOpenfile.Location = new System.Drawing.Point(401, 16);
+            this.btOpenfile.Name = "btOpenfile";
+            this.btOpenfile.Size = new System.Drawing.Size(75, 23);
+            this.btOpenfile.TabIndex = 0;
+            this.btOpenfile.Text = "选择";
+            this.btOpenfile.UseVisualStyleBackColor = true;
+            this.btOpenfile.Click += new System.EventHandler(this.btOpenfile_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 358);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PDF 添加水印";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialogPDF;
+        private System.Windows.Forms.Button btMark;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbPdfFiles;
+        private System.Windows.Forms.Button btOpenfile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PDFWatermark
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btOpenfile_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog fileDialog = new OpenFileDialog();
+
+            //openFileDialogPDF.Multiselect = true;
+            //openFileDialogPDF.Title = "请选择文件";
+            //openFileDialogPDF.Filter = "所有 PDF 文件|*.pdf"; //设置要选择的文件的类型
+            if (openFileDialogPDF.ShowDialog() == DialogResult.OK)
+            {
+                //System.IO.Path.GetFullPath(openFileDialogPDF.FileName);//绝对路径
+                //System.IO.Path.GetExtension(openFileDialogPDF.FileName);//文件扩展名
+                //System.IO.Path.GetFileNameWithoutExtension(openFileDialogPDF.FileName);//文件名没有扩展名
+                //System.IO.Path.GetFileName(openFileDialogPDF.FileName);//得到文件名
+                //System.IO.Path.GetDirectoryName(openFileDialogPDF.FileName);//得到路径
+
+                // tbFilename.Text = openFileDialogPDF.FileName;//返回文件的完整路径
+                // 清除 listbox 项目
+                lbPdfFiles.Items.Clear();
+                // 填充 listbox
+                foreach (String file in openFileDialogPDF.FileNames)
+                {                    
+                    try
+                    {
+                        lbPdfFiles.Items.Add(file);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("文件打开错误！");
+                    }
+                }
+            }
+        }
+
+        private void btMark_Click(object sender, EventArgs e)
+        {
+            string msg = string.Empty;
+            if (lbPdfFiles.Items.Count > 0)
+            {
+                foreach (String file in lbPdfFiles.Items)
+                {
+                    string outFile = System.IO.Path.GetDirectoryName(file) + "\\水印" + System.IO.Path.GetFileName(file);
+                    bool convertIsok = add_watermark(file, outFile);
+                    if (convertIsok)
+                    {
+                        msg += file + "\n";
+                        //MessageBox.Show("PDF 水印添加成功！");
+                    }
+                }
+                MessageBox.Show(msg + "已成功添加水印！");
+            }
+            else
+            {
+                MessageBox.Show("请选择需要添加水印的 PDF 文件！");
+            }
+        }
+
+        //给单个文件添加水印
+        //所有参数均为全路径文件名
+        bool add_watermark(string srcPdf, string dstPdf)//, string imagepath)
+        {
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+                Properties.Resources.HQWatermark.Save(ms, Properties.Resources.HQWatermark.RawFormat);
+                byte[] byteImage = new Byte[ms.Length];
+                byteImage = ms.ToArray();
+
+                // iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(imagepath);
+                iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(byteImage);
+                PdfReader reader = new PdfReader(srcPdf);
+                PdfStamper stamp = new PdfStamper(reader, new FileStream(dstPdf, FileMode.Create));
+
+
+                PdfGState gs1 = new PdfGState();
+                gs1.FillOpacity = 0.5f;// 透明度设置
+
+                PdfContentByte page;
+                float width = reader.GetPageSize(1).Width;
+                float height = reader.GetPageSize(1).Height;
+
+                //img.SetAbsolutePosition(img.ScaledWidth, img.ScaledHeight); // 坐标
+                img.ScalePercent(30);//依照比例缩放
+                img.SetAbsolutePosition((width - img.ScaledWidth) / 2, (height - img.ScaledHeight) / 2);
+                //img.Rotation = -20;// 旋转 弧度
+                //img.RotationDegrees = 45;// 旋转 角度
+                // img.scaleAbsolute(200,100);//自定义大小            
+
+                int num = reader.NumberOfPages;
+                for (int i = 1; i <= num; ++i)
+                {
+                    // page = stamp.GetUnderContent(i); // 水印在之前文本下
+                    page = stamp.GetOverContent(i); //水印在之前文本上
+
+                    page.SetGState(gs1);
+                    page.AddImage(img);
+
+                    //img.SetAbsolutePosition(width - img.Width - margin, height - img.Height - margin);
+                    //page.AddImage(img);
+                }
+
+                stamp.Close();
+                reader.Close();
+
+                return true;
+            }
+            catch(IOException ioex)
+            {
+                MessageBox.Show(srcPdf + "\n无法保存添加水印文件，可能是文件已经打开，请关闭！\n其它文件将继续添加水印！");
+                return false;
+            }
+        }        
+    }
+}
